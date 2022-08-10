90bb47edb618a75a2b0e238f27c38b64945ba927
Driven Education <marcio.garrido.jr@gmail.com>
2022-08-10 19:51:28 -0300

    feat: implementing a function for insert and show posts on the aplication

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
+import postsRouter from "./postRouter.js";
 import userRouter from "./userRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(userRouter);
+router.use(postsRouter);
 
 export default router;
