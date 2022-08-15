4d7a7095847cd9bdafe9a853ed8c7114cc76a234
vcsil <vcsilva@poli.ufrj.br>
2022-08-15 17:47:14 -0300

    chore: Prepare for deploy

--- src/db/db.js
+++ src/db/db.js
@@ -6,11 +6,10 @@ dotenv.config();
 const { Pool } = pg;
 
 const databaseConfig = {
-    host: process.env.HOST_POSTGRES,
-    port: process.env.PORT_POSTGRES,
-    user: process.env.USER_POSTGRES,
-    password: process.env.PASSWORD_POSTGRES,
-    database: process.env.DATABASE_POSTGRES
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 };
 
 const connection = new Pool(databaseConfig);
