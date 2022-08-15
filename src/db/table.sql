9a2a6b97088c76b5c1474bf6f9d4d8812aa19c62
vcsil <vcsilva@poli.ufrj.br>
2022-08-15 17:43:22 -0300

    chore: type table sql

--- src/db/table.sql
+++ src/db/table.sql
@@ -2,7 +2,7 @@ CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR(40) NOT NULL,
     "email" TEXT NOT NULL UNIQUE,
-    "password" TEXT(40) NOT NULL,
+    "password" TEXT NOT NULL,
     "profile_img_url" TEXT NOT NULL,
     "created_at" TIMESTAMP DEFAULT NOW()
 );
